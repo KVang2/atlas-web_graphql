11fd9779a54b0e9175cab2c4978553b29e0c652f
kvang2 <kevinvaj88@hotmail.com>
2024-11-06 20:00:08 -0600

    update schema.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,6 +3,23 @@ const { GraphQLObjectType,
     GraphQLInt
 } = require('graphql');
 
+// 2 dummy data array
+const tasks = [
+    {
+        id: '1',
+        title: 'Create your first webpage',
+        weight: '1',
+        description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+    },
+    {
+        id: '2',
+        title: 'Structure your webpage',
+        weight: '1',
+        description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+    },
+];
+
+// TaskType GraphQL object
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: {
@@ -12,19 +29,20 @@ const TaskType = new GraphQLObjectType({
     },
 });
 
+// RootQuery
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
     task: {
             type: TaskType,
-            args: { 
-                id: "1" { type: GraphQLString },
-                id: "2" { type: GraphQLString },
+            args: { id: { type: GraphQLString } },
             resolve(parent, args) {
-
+                return _.find(tasks, { id: args.id });
             }
         }
     }
 });
 
-module.exports = { TaskType, schema };
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
