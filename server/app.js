d2ce04003d68e6ed7d615fab08246356cd2c9b6a
kvang2 <kevinvaj88@hotmail.com>
2024-11-11 21:03:31 -0600

    update to task 5 project type

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
-const schema = require('schema');
+const schema = require('../server/schema/schema');
 
 const app = express();
 
 app.use('/graphql',graphqlHTTP({
+  schema: schema,
+  graphiql: true,
 }));
 
 app.listen(4000,()=>{
